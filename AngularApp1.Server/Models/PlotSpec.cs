d2a7c395d4f59f87700e984ab685fa31f7e4e0c1
hradoev@abv.bg <hradoev@abv.bg>
2026-01-26 16:21:43 +0200

    plot spec

---
+++ AngularApp1.Server/Models/PlotSpec.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AngularApp1.Server.Models
+{
+    public class PlotSpec
+    {
+        [Required]
+        public string PlotUrl { get; set; }
+
+        [Required]
+        public string PlotName { get; set; }
+
+    }
+}
